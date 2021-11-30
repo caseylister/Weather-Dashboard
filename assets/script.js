87fd7afcd11d0a062bddaceb7a8896f42bee321a
caseylister <caseynlister@gmail.com>
2021-11-30 15:29:03 -0500

    create global variables

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+var citiesListArr = [];
+var numOfCities = 9;
+var myApi = "appid=4656869a8d8816608a5b74aab68efafa";
+var unit = "units=imperial";
+var dailyWeather = "https://api.openweathermap.org/data/2.5/weather?q=";
+var dailyUV = "https://api.openweathermap.org/data/2.5/uvi?";
+var forecastWeather = "https://api.openweathermap.org/data/2.5/onecall?";
+
+var searchCityForm = $("#searchCityForm");
+var searchedCities = $("#cities");
